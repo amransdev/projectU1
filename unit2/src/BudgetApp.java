eac5825fe891947b4d7ad6936aea6e371205f315
Amran Salaad <amransdev@gmail.com>
2025-10-09 14:29:37 +0100

    Print welcome header and main menu

--- unit2/src/BudgetApp.java
+++ unit2/src/BudgetApp.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class BudgetApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the budget app!");
+        System.out.println("==================================");
+        System.out.println(   "Welcome to Budget Manager CLI");
+        System.out.println("===================================\n");
+
+        System.out.println("Main Menu");
+        System.out.println("----------");
+
+        System.out.println("1) Add an entry (income or expense)");
+        System.out.println("2) Remove an entry");
+        System.out.println("3) View all entries");
+        System.out.println("4) Exit");
+
+        System.out.println("Choose an option (1-4:  ");
     }
 }
